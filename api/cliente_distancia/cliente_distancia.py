c288efb3e94aba544eff360f98598957ddb045ae
Bianca-Jennifer <bianca.kalyse@gmail.com>
2026-06-03 22:22:05 -0300

    Alterações;

--- api/cliente_distancia/cliente_distancia.py
+++ api/cliente_distancia/cliente_distancia.py
@@ -1,8 +1,9 @@
 import requests
 import os
-import api.services.Lugar as Lugar
+import api.classes.lugar as Lugar
+from api.services.leitor_de_arquivo import ler_arquivo_de_lugares
 
-OSRM_URL = os.getenv("OSRM_URL", "http://localhost:5000")
+OSRM_URL = os.getenv("OSRM_URL", "http://10.0.0.119:5000")
 
 
 def obter_distancia_entre_dois_pontos(lugar1, lugar2):
@@ -28,11 +29,12 @@ def obter_caminho_entre_dois_pontos(lugar1, lugar2):
 
 def gerar_matriz_de_distancias(lugares):
     coordenadas_lugares = ";".join(lugar.to_osrm() for lugar in lugares)
-    url = f"{OSRM_URL}/table/v1/driving/{coordenadas_lugares}?overview=false"
+    url = f"{OSRM_URL}/table/v1/driving/{coordenadas_lugares}?annotations=distance"
 
     resposta = requests.get(url).json()
-    
+
     return resposta["distances"]
+
  
 def gerar_rota_final(lugares_solucao):
     coordenadas_lugares = ";".join(lugar.to_osrm() for lugar in lugares_solucao)
@@ -41,3 +43,6 @@ def gerar_rota_final(lugares_solucao):
     resposta = requests.get(url).json()
     
     return resposta["routes"][0]["geometry"]
+
+
+
--- api/services/leitor_de_arquivo.py
+++ api/services/leitor_de_arquivo.py
@@ -1,5 +1,5 @@
 import csv
-from Lugar import Lugar
+from api.classes.lugar import Lugar
 def ler_arquivo_de_lugares(arquivo_csv):
     lugares = []
 
@@ -17,10 +17,13 @@ def ler_arquivo_de_lugares(arquivo_csv):
             )    
     return lugares
 
-lugares = ler_arquivo_de_lugares("teste.csv")
+lugares = ler_arquivo_de_lugares("api/services/teste.csv")
 
 print(lugares)
 cont = 0
 for l in lugares:
     print(l.nome, l.latitude, l.longitude, l.tipo, cont)
     cont+=1
+
+print("----------")
+print(lugares[0].nome, lugares[0].to_osrm())    
