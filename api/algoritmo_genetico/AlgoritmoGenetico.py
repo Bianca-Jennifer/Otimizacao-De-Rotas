a83552af3d15355cc9ca09a2a0659d90ebb3f635
Alex Eduardo <alexeduardo701@gmail.com>
2026-06-05 18:15:54 -0300

    Importando metodo melhor individuo e torneio

--- api/algoritmo_genetico/AlgoritmoGenetico.py
+++ api/algoritmo_genetico/AlgoritmoGenetico.py
@@ -1,3 +1,4 @@
+import random
 from api.classes.definicao_problema import DefinicaoProblema
 
 class AlgoritmoGeneticos:
@@ -5,3 +6,20 @@ class AlgoritmoGeneticos:
         self.limite_geracoes = limite_geracoes
         self.tamanho_populacao = tamanho_populacao
         self.definicao_problema = definicao_problema
+
+    def obter_melhor_individuo(self, populacao_atual):
+        if not populacao_atual:
+            return None
+        
+        melhor_individuo = populacao_atual[0]
+        
+        for individuo in populacao_atual[1:]:
+            if individuo.fitness < melhor_individuo.fitness:
+                melhor_individuo = individuo
+
+        return melhor_individuo
+    
+    def torneio(self, populacao, tamanho_torneio):
+        participantes = random.sample(populacao, tamanho_torneio)
+        melhor_participante = min(participantes, key=lambda individuo: individuo.fitness)
+        return melhor_participante
