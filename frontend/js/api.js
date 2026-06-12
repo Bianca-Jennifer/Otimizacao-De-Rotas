70c664d0c577010126de4f9188febea1779ea715
Vinicius Kaua <visilva2435@gmail.com>
2026-06-12 18:04:11 -0300

    feat: api.js e resultado.json para fazer o teste

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -1,51 +1,18 @@
 async function enviarDadosDaRota(dados) {
-    const enderecoApi = 'http://localhost:8000/api/otimizar';
+
+    const urlDoArquivo = './js/resultado.json'; 
 
     try {
-        const resposta = await fetch(enderecoApi, {
-            method: 'POST',
-            body: dados
-        });
+        const resposta = await fetch(urlDoArquivo);
 
         if (!resposta.ok) {
-            throw new Error('Erro de comunicacao com a API');
+            throw new Error('Não foi possível carregar o arquivo resultado.json');
         }
 
-        const resultado = await resposta.json();
-        return resultado;
+        return await resposta.json();
         
     } catch (erro) {
+        console.error("Erro ao ler o arquivo local:", erro);
         throw erro;
     }
 }
-
-
-// async function enviarDadosDaRota(dados) {
-//     // Simulando o tempo de processamento da API (1.5 segundos)
-//     return new Promise(resolver => {
-//         setTimeout(() => {
-//             console.log("Mock: Simulando trajeto linear de A para B (sem retorno).");
-            
-//             resolver({
-//                 distancia: "4.2",
-//                 combustivel: "0.3",
-//                 geracoes: 150,
-//                 rota_nomes: [
-//                     "Ponto A - Origem (Midway Mall)",
-//                     "Parada 1 (Cruzamento Bernardo Vieira)",
-//                     "Parada 2 (Portugal Center)",
-//                     "Parada 3 (Arena das Dunas)",
-//                     "Ponto B - Destino Final (UFRN)"
-//                 ],
-//                 // Coordenadas traçando um caminho quase reto descendo a avenida
-//                 coordenadas: [
-//                     [-5.8118, -35.2062], // Origem
-//                     [-5.8160, -35.2075], // Parada 1
-//                     [-5.8210, -35.2095], // Parada 2
-//                     [-5.8267, -35.2126], // Parada 3
-//                     [-5.8320, -35.2150]  // Destino
-//                 ]
-//             });
-//         }, 1500);
-//     });
-// }
