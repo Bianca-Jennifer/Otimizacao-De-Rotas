c9c5fdaf6a04514625a23efbeaf511d5091aa893
Vinicius Kaua <visilva2435@gmail.com>
2026-06-13 14:07:23 -0300

    refactor: API

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -1,18 +1,40 @@
+// async function enviarDadosDaRota(dados) {
+
+//     const urlDoArquivo = './js/resultado.json'; 
+
+//     try {
+//         const resposta = await fetch(urlDoArquivo);
+
+//         if (!resposta.ok) {
+//             throw new Error('Não foi possível carregar o arquivo resultado.json');
+//         }
+
+//         return await resposta.json();
+        
+//     } catch (erro) {
+//         console.error("Erro ao ler o arquivo local:", erro);
+//         throw erro;
+//     }
+// }
+
 async function enviarDadosDaRota(dados) {
 
-    const urlDoArquivo = './js/resultado.json'; 
+    const urlDaApi = 'http://localhost:5000/processar_rota'; 
 
     try {
-        const resposta = await fetch(urlDoArquivo);
+        const resposta = await fetch(urlDaApi, {
+            method: 'POST',
+            body: dados
+        });
 
         if (!resposta.ok) {
-            throw new Error('Não foi possível carregar o arquivo resultado.json');
+            throw new Error('Erro ao processar rota no servidor');
         }
 
         return await resposta.json();
-        
+
     } catch (erro) {
-        console.error("Erro ao ler o arquivo local:", erro);
+        console.error("Falha na comunicação com a API:", erro);
         throw erro;
     }
 }
