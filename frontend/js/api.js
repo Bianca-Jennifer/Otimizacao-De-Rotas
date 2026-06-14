6d14f548f93179691862b26e20f1b576e19e76ff
Vinicius Kaua <visilva2435@gmail.com>
2026-06-14 10:54:30 -0300

    removendo mock

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -1,22 +1,3 @@
-// async function enviarDadosDaRota(dados) {
-
-//     const urlDoArquivo = './js/resultado.json'; 
-
-//     try {
-//         const resposta = await fetch(urlDoArquivo);
-
-//         if (!resposta.ok) {
-//             throw new Error('Não foi possível carregar o arquivo resultado.json');
-//         }
-
-//         return await resposta.json();
-        
-//     } catch (erro) {
-//         console.error("Erro ao ler o arquivo local:", erro);
-//         throw erro;
-//     }
-// }
-
 async function enviarDadosDaRota(dados) {
 
     const urlDaApi = 'http://localhost:5000/processar_rota'; 
