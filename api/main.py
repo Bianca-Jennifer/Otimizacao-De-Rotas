4417d71ef38192a58b0921cc43c5d742836566ce
Kelpy Lima <kelpy.a.lima@gmail.com>
2026-06-14 17:17:36 -0300

    chore: move os demais arquivos relevantes para a execução da api para /api

--- api/main.py
+++ api/main.py
@@ -106,4 +106,4 @@ def executar_algoritmo_completo(
         raise HTTPException(status_code=500, detail=f"Erro durante a execução: {str(e)}")
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("api.main:app", host="0.0.0.0", port=8000, reload=True)
